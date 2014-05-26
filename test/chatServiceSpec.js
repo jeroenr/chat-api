094fc21d37e10671a6d3af99e25f01a00ee5a567
Jeroen Rosenberg <jeroen.rosenberg@gmail.com>
2014-05-26 22:14:00 +0200

    Use stubs

--- test/chatServiceSpec.js
+++ test/chatServiceSpec.js
@@ -1,15 +1,19 @@
 var chai = require('chai')
-  , spies = require('chai-spies');
-
-chai.use(spies);
+  , spies = require('chai-spies')
+  , sinon = require("sinon")
+  , sinonChai = require("sinon-chai");
 
 var should = chai.should()
   , expect = chai.expect;
 
+chai.use(sinonChai);
+chai.use(spies);
+
+
 
 // spies
-var redisPublishSpy = chai.spy()
-  , redisOnSpy = chai.spy();
+var redisPublishSpy = sinon.spy()
+  , redisOnSpy = sinon.spy();
 
 var winston = require('winston');
 var logger = new (winston.Logger)({
@@ -39,14 +43,14 @@ var chatService = require("../lib/chatService.js")({}, mockModels, {});
 describe("ChatService", function(){
 	describe("#onChatMessage", function(){
 		it("should publish message to redis channel", function(){
-			expect(redisOnSpy).to.have.been.called();
+			redisOnSpy.should.have.been.calledWith("message");
 		});
 	});
 
 	describe("#broadcastChatMessage()", function(){
 		it("should publish message to redis channel", function(){
-			var results = chatService.broadcastChatMessage("message");
-			expect(redisPublishSpy).to.have.been.called();
+			var results = chatService.broadcastChatMessage({message: "Hello", room: "room 1"});
+			redisPublishSpy.should.have.been.calledWith('chatmessage','{"message":"Hello","room":"room 1"}');
 		});
 	});
 });
