00c8f77f75d0073d5d7d076325cb696468d0cafa
Jeroen Rosenberg <jeroen.rosenberg@gmail.com>
2014-05-26 23:06:27 +0200

    Test redis multi

--- test/chatServiceSpec.js
+++ test/chatServiceSpec.js
@@ -1,7 +1,8 @@
 var chai = require('chai')
   , spies = require('chai-spies')
   , sinon = require("sinon")
-  , sinonChai = require("sinon-chai");
+  , sinonChai = require("sinon-chai")
+  , winston = require('winston');
 
 var should = chai.should()
   , expect = chai.expect;
@@ -9,22 +10,17 @@ var should = chai.should()
 chai.use(sinonChai);
 chai.use(spies);
 
-
-
-// spies
+// global spies
 var redisPublishSpy = sinon.spy()
   , redisOnSpy = sinon.spy();
 
-var winston = require('winston');
-var logger = new (winston.Logger)({
-    transports: [new (winston.transports.Console)({ json: false, timestamp: true })], 
-    exceptionHandlers: [new (winston.transports.Console)({ json: false, timestamp: true })], 
-    exitOnError: false
-  });
-
 var mockModels = {
 	log: {
-		logger: logger
+		logger: new (winston.Logger)({
+		    transports: [new (winston.transports.Console)({ json: false, timestamp: true })], 
+		    exceptionHandlers: [new (winston.transports.Console)({ json: false, timestamp: true })], 
+		    exitOnError: false
+		})
 	},
 	redis: {
 		chatMessageChannel: { 
@@ -32,7 +28,8 @@ var mockModels = {
 		},
 		client: {
 			publish: redisPublishSpy,
-			zrange: sinon.stub()
+			zrange: sinon.stub(),
+			multi: sinon.stub()
 		}
 	}
 }
@@ -43,9 +40,14 @@ var mockPrimus = {
 
 var chatService = require("../lib/chatService.js")({}, mockModels, mockPrimus);
 
-
-
 describe("ChatService", function(){
+	afterEach(function(done){
+	    redisPublishSpy.reset();
+	    redisOnSpy.reset();
+	    done();
+	});
+
+
 	describe("#onChatMessage", function(){
 		it("subscribe to messages on redis channel", function(){
 			redisOnSpy.should.have.been.calledWith("message");
@@ -61,12 +63,31 @@ describe("ChatService", function(){
 
 	describe("#deliverChatMessage()", function(){
 		it("should deliver chat messages to connected recipients", function(){
-
-			var sparkMock = { userId: "id2", send: sinon.spy()};
-			mockModels.redis.client.zrange.withArgs("rooms:room1",0,-1).callsArgWith(3, undefined, ["id1","id2"]);
-			mockPrimus.forEach.callsArgWith(0, sparkMock, "id2", []);
+			var sparkMock = { userId: "user2", send: sinon.spy()};
+			mockModels.redis.client.zrange.withArgs("rooms:room1",0,-1).callsArgWith(3, undefined, ["user1","user2"]);
+			mockPrimus.forEach.callsArgWith(0, sparkMock, "user2", []);
 			var results = chatService.deliverChatMessage({message: "Hello", room: "room1"});
 			sparkMock.send.should.have.been.calledWith('message', "Hello");
+		});
+	});
+
+	describe("#leaveAllRooms()", function(){
+		it("should make user leave all specified rooms", function(){
+			var cb = sinon.spy();
+			var execStub = sinon.stub();
+			execStub.callsArgWith(0, undefined,["1,2"]);
+
+			mockModels.redis.client.multi.withArgs([
+				["zrem","rooms:room1","user1"],
+				["zrem","rooms:room2","user1"],
+				["zrem","rooms:for:user1",["room1","room2"]]
+			]).returns({
+				exec: execStub
+			});
+			var results = chatService.leaveAllRooms("user1", ["room1","room2"], cb);
+
+			cb.should.have.been.calledOnce;
+			redisPublishSpy.should.have.been.calledTwice;
 		});
 	});
 
